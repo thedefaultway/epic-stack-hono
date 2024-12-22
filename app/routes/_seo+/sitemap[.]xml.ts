d3592d161d4d01ba4241f02adc5d59198ce60d12
Hakim Gueye <beatsjordy@gmail.com>
2024-12-22 12:39:32 +0000

    Update sitemap[.]xml.ts

--- app/routes/_seo+/sitemap[.]xml.ts
+++ app/routes/_seo+/sitemap[.]xml.ts
@@ -1,10 +1,15 @@
 import { generateSitemap } from '@nasa-gcn/remix-seo'
-import { type ServerBuild, type LoaderFunctionArgs } from 'react-router';
+import { type ServerBuild, type ServerRouter } from 'react-router'
+import { type RouteManifest } from 'remix-flat-routes'
 import { getDomainUrl } from '#app/utils/misc.tsx'
+import { type Route } from './+types/sitemap[.]xml.ts'
 
-export async function loader({ request, context }: LoaderFunctionArgs) {
-	const serverBuild = (await context.serverBuild) as { build: ServerBuild }
-	return generateSitemap(request, serverBuild.build.routes, {
+export async function loader({ request, context }: Route.LoaderArgs) {
+	const serverBuild = (await context.serverBuild) as ServerBuild
+	const routes = serverBuild?.routes as RouteManifest<
+		Omit<typeof ServerRouter, 'children'>
+	>
+	return generateSitemap(request, routes, {
 		siteUrl: getDomainUrl(request),
 		headers: {
 			'Cache-Control': `public, max-age=${60 * 5}`,
